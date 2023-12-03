653044a2d0cac005ac87016f3be9162ac2d23832
laronichill <laronichill@gmail.com>
2023-12-03 13:31:20 -0500

    RoadSpawns and Car Controller fixes.

--- CarRunner/Assets/Scripts/Playable/CarController.cs
+++ CarRunner/Assets/Scripts/Playable/CarController.cs
@@ -10,26 +10,37 @@ public class CarController : MonoBehaviour {
 
     public List<AxleInfo> axleInfos; // the information about each individual axle
     public float maxMotorTorque; // maximum torque the motor can apply to wheel
+    public float maxBrakeTorque; // maximum torque the motor can apply to wheel
     public float maxSteeringAngle; // maximum steer angle the wheel can have
     private basicController Controls;
+
+    // Center of masss
+    public Vector3 com;
+    public Rigidbody rb;
+
     void Start() {
         Controls = new basicController();
+
+        rb = GetComponent<Rigidbody>();
+        rb.centerOfMass = com;
     }   
     void FixedUpdate()
     {
-        float motor = maxMotorTorque * Controls.vertical();
-        float steering = maxSteeringAngle * Controls.horizontal();
-
+        float motor = maxMotorTorque * Controls.rightTrig();
+        float brakes = maxBrakeTorque * Controls.leftShoulder();
+        float steering = maxSteeringAngle * Controls.leftStick();
         foreach (AxleInfo axleInfo in axleInfos) {
             if (axleInfo.steering) {
                 axleInfo.leftWheel.steerAngle = steering;
                 axleInfo.rightWheel.steerAngle = steering;
             }
             if (axleInfo.motor) {
+                    
                 axleInfo.leftWheel.motorTorque = motor;
                 axleInfo.rightWheel.motorTorque = motor;
-                Debug.Log(axleInfo.leftWheel.motorTorque);
             }
+            axleInfo.leftWheel.brakeTorque = brakes;
+            axleInfo.rightWheel.brakeTorque = brakes;
         }
     }
 }
@@ -43,19 +54,25 @@ public class AxleInfo {
 }
 
 public class basicController {
-    private ButtonControl _throttleBind = Gamepad.current.rightTrigger;
-    private ButtonControl _reverseBind = Gamepad.current.leftTrigger;
-    private ButtonControl _downShiftBind = Gamepad.current.leftShoulder;
-    private ButtonControl _upShiftBind = Gamepad.current.rightShoulder;
-    private StickControl _steerStick = Gamepad.current.leftStick;
-    private StickControl _freeLookStick = Gamepad.current.rightStick;
-    public bool isPresent() => Gamepad.current != null;
-    public float vertical() => (isPresent()) ? _throttleBind.ReadValue() - _reverseBind.ReadValue() : Input.GetAxis("Vertical");
-    public float horizontal() => (isPresent()) ? _steerStick.x.ReadValue() : Input.GetAxis("Horizontal");
+    private ButtonControl _rightTrig = Gamepad.current.rightTrigger;
+    private ButtonControl _leftTrig = Gamepad.current.leftTrigger;
 
-/*     public bool downShift() => ((_downShiftKey || _downShiftBind.wasPressedThisFrame) && _gearboxObj.currentGear > 1);
-    public bool upShift() => ((_upShiftKey || _upShiftBind.wasPressedThisFrame) && _gearboxObj.currentGear < _gearboxObj.gearCount); */
-    public float[] freeLookCoords() => new float[] { _freeLookStick.x.ReadValue(), _freeLookStick.y.ReadValue() };
+    private ButtonControl _leftShoulder = Gamepad.current.leftShoulder;
+    private ButtonControl _rightShoulder = Gamepad.current.rightShoulder;
+
+    private StickControl _leftStick = Gamepad.current.leftStick;
+    private StickControl _rightStick = Gamepad.current.rightStick;
+
+    public bool isPresent() => Gamepad.current != null;
+    // R2
+    public float rightTrig() => (isPresent()) ? _rightTrig.ReadValue() - _leftTrig.ReadValue() : Input.GetAxis("Vertical");
+    // L3
+    public float leftStick() => (isPresent()) ? _leftStick.x.ReadValue() : Input.GetAxis("Horizontal");
+    // L2
+    public int leftShoulder() => (isPresent() && _leftShoulder.isPressed) ? 1 : 0;
+    // public bool downShift() => ((_downShiftKey || _downShiftBind.wasPressedThisFrame) && _gearboxObj.currentGear > 1);
+    // public bool upShift() => ((_upShiftKey || _upShiftBind.wasPressedThisFrame) && _gearboxObj.currentGear < _gearboxObj.gearCount);
+    public float[] rightStick() => new float[] { _rightStick.x.ReadValue(), _rightStick.y.ReadValue() };
 
     public basicController()
     {
--- CarRunner/Assets/Scripts/Roads/ProceduralRoadScript.cs
+++ CarRunner/Assets/Scripts/Roads/ProceduralRoadScript.cs
@@ -1,18 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using PathCreation;
+using System.Linq;
+using Unity.VisualScripting;
 
 public class ProceduralRoadScript : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Vector3 StartingPoint;
+    public PathCreator Left90;
+    public PathCreator Right90;
+    public PathCreator Straight;
+    private GameObject player;
+    private List<PathCreator> roadSectionList = new List<PathCreator>();
+    private int rotation; // 0 = Nord; 1=Est; 2=Sud; 3=Ouest; 
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
+        PathCreator startRoad = Instantiate(Straight, new Vector3(player.transform.position.x, 0, player.transform.position.z), Quaternion.identity);
+        roadSectionList.Add(startRoad);
+
     }
 
     // Update is called once per frame
-    void Update()
+
+
+    void FixedUpdate()
     {
+        if (Vector3.Distance(player.transform.position, roadSectionList.Last().transform.position) <= 40) {
+            if (roadSectionList.Count > 5) {
+                RemoveFirstRoadSection();
+            }
+            AddEndSection();
+        }
+    }
+
+    private void AddEndSection()
+    {
+        Vector3 lastRoadTransform = roadSectionList.Last().path.GetPoint(1);
+        PathCreator roadSection = Instantiate(GetRandomSection(), lastRoadTransform, Quaternion.identity);
+        StartingPoint = roadSection.path.GetPoint(0);
+        Vector3 RealPositionRoad = roadSection.transform.position + (lastRoadTransform - StartingPoint);
+        roadSection.transform.position = RealPositionRoad;
+        roadSectionList.Add(roadSection);
+
+    }
+    private void RemoveFirstRoadSection()
+    {
+        PathCreator lastRoad = roadSectionList.First();
+        roadSectionList.RemoveAt(0);
+        Destroy(lastRoad);
+    }     
+    private PathCreator GetRandomSection()
+    {
+        PathCreator[] objects = { Left90, Right90, Straight };
         
+        return objects[Random.Range(0, objects.Length)];
     }
 }
