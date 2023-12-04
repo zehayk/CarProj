bed27067233aecff7efdc5264542da3692733f93
laronichill <laronichill@gmail.com>
2023-12-04 05:35:19 -0500

    Couple of fixes

--- CarRunner/Assets/Scripts/Roads/ProceduralRoadScript.cs
+++ CarRunner/Assets/Scripts/Roads/ProceduralRoadScript.cs
@@ -4,23 +4,21 @@ using UnityEngine;
 using PathCreation;
 using System.Linq;
 using Unity.VisualScripting;
+using System.IO;
 
 public class ProceduralRoadScript : MonoBehaviour
 {
     // Start is called before the first frame update
-    public Vector3 StartingPoint;
-    public PathCreator Left90;
-    public PathCreator Right90;
-    public PathCreator Straight;
+    public Vector3 FirstPoint;
+    public PathCreator BaseRoad;
     private GameObject player;
-    private List<PathCreator> roadSectionList = new List<PathCreator>();
     private int rotation; // 0 = Nord; 1=Est; 2=Sud; 3=Ouest; 
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
-        PathCreator startRoad = Instantiate(Straight, new Vector3(player.transform.position.x, 0, player.transform.position.z), Quaternion.identity);
-        roadSectionList.Add(startRoad);
-
+        PathCreator startRoad = Instantiate(BaseRoad, new Vector3(player.transform.position.x, 0, player.transform.position.z), Quaternion.identity);
+        BaseRoad.bezierPath.GlobalNormalsAngle = 90f;
+        BaseRoad.TriggerPathUpdate();
     }
 
     // Update is called once per frame
@@ -28,34 +26,32 @@ public class ProceduralRoadScript : MonoBehaviour
 
     void FixedUpdate()
     {
-        if (Vector3.Distance(player.transform.position, roadSectionList.Last().transform.position) <= 40) {
-            if (roadSectionList.Count > 5) {
-                RemoveFirstRoadSection();
-            }
+        if (Vector3.Distance(player.transform.position, BaseRoad.path.GetPoint(1)) < 20f) {
             AddEndSection();
+        }
+        if (Vector3.Distance(player.transform.position, BaseRoad.path.GetPoint(0)) >= 30f)
+        {
+            RemoveFirstRoadSection();
         }
     }
 
     private void AddEndSection()
     {
-        Vector3 lastRoadTransform = roadSectionList.Last().path.GetPoint(1);
-        PathCreator roadSection = Instantiate(GetRandomSection(), lastRoadTransform, Quaternion.identity);
-        StartingPoint = roadSection.path.GetPoint(0);
-        Vector3 RealPositionRoad = roadSection.transform.position + (lastRoadTransform - StartingPoint);
-        roadSection.transform.position = RealPositionRoad;
-        roadSectionList.Add(roadSection);
-
+        // PathCreator roadSection = Instantiate(GetRandomSection(), lastRoadTransform, Quaternion.identity);
+        // Vector3 RealPositionRoad = (lastPoint - StartingPoint);
+        BaseRoad.bezierPath.AddSegmentToEnd(GetRandomPoint(BaseRoad.path.GetPoint(1)));
+        BaseRoad.TriggerPathUpdate();
     }
     private void RemoveFirstRoadSection()
     {
-        PathCreator lastRoad = roadSectionList.First();
-        roadSectionList.RemoveAt(0);
-        Destroy(lastRoad);
-    }     
-    private PathCreator GetRandomSection()
+    }
+    private Vector3 GetRandomPoint(Vector3 lastPointX)
     {
-        PathCreator[] objects = { Left90, Right90, Straight };
-        
-        return objects[Random.Range(0, objects.Length)];
+        float NumX = Random.Range(0, 20);
+        float NumZ = 40 - NumX;
+        // Use lastPointX as an offset for generating a new random point
+        Vector3 lastPointAdd = new Vector3(lastPointX.x + NumX, lastPointX.y, lastPointX.z + NumZ);
+        Debug.Log(lastPointAdd);
+        return lastPointAdd;
     }
 }
