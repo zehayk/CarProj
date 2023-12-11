6918a6937f76d460ee82ce74524db8c0ace46db8
laronichill <laronichill@gmail.com>
2023-12-11 14:15:22 -0500

    Fixes

--- CarRunner/Assets/Scripts/Playable/CarController.cs
+++ CarRunner/Assets/Scripts/Playable/CarController.cs
@@ -19,7 +19,6 @@ public class CarController : MonoBehaviour {
     public Rigidbody rb;
 
     void Start() {
-        Debug.Log("Game has started");
         Controls = new basicController();
 
         rb = GetComponent<Rigidbody>();
@@ -27,14 +26,10 @@ public class CarController : MonoBehaviour {
     }   
     void FixedUpdate()
     {
-        //float motor = maxMotorTorque * Controls.rightTrig();
-        //float brakes = maxBrakeTorque * Controls.leftShoulder();
-        //float steering = maxSteeringAngle * Controls.leftStick();
-        // Adjust the following input names based on your needs
+        float motor = maxMotorTorque * Controls.rightMinusLeftTrig();
+        float brakes = maxBrakeTorque * Controls.leftShoulder();
+        float steering = maxSteeringAngle * Controls.leftStick();
 
-        float motor = maxMotorTorque * Input.GetAxis("Vertical");
-        float brakes = Input.GetKey(KeyCode.Space) ? maxBrakeTorque : 0f;
-        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
 
         foreach (AxleInfo axleInfo in axleInfos) {
             if (axleInfo.steering) {
@@ -56,8 +51,8 @@ public class CarController : MonoBehaviour {
 public class AxleInfo {
     public WheelCollider leftWheel;
     public WheelCollider rightWheel;
-    public bool motor; // is this wheel attached to motor?
-    public bool steering; // does this wheel apply steer angle?
+    public bool motor;
+    public bool steering;
 }
 
 public class basicController {
@@ -72,11 +67,13 @@ public class basicController {
 
     public bool isPresent() => Gamepad.current != null;
     // R2
-    public float rightTrig() => (isPresent()) ? _rightTrig.ReadValue() - _leftTrig.ReadValue() : Input.GetAxis("Vertical");
+
+    public float rightMinusLeftTrig() => (isPresent()) ? _rightTrig.ReadValue() - _leftTrig.ReadValue() : Input.GetAxis("Vertical");
     // L3
     public float leftStick() => (isPresent()) ? _leftStick.x.ReadValue() : Input.GetAxis("Horizontal");
     // L2
-    public int leftShoulder() => (isPresent() && _leftShoulder.isPressed) ? 1 : 0;
+
+    public int leftShoulder() => ((isPresent()) ? _leftShoulder.isPressed : Input.GetKey(KeyCode.Space)) ? 1 : 0;
     // public bool downShift() => ((_downShiftKey || _downShiftBind.wasPressedThisFrame) && _gearboxObj.currentGear > 1);
     // public bool upShift() => ((_upShiftKey || _upShiftBind.wasPressedThisFrame) && _gearboxObj.currentGear < _gearboxObj.gearCount);
     public float[] rightStick() => new float[] { _rightStick.x.ReadValue(), _rightStick.y.ReadValue() };
