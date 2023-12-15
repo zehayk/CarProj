0f4dba533e5da06bb6a226bcbcdacb3442a2221d
tree <62469167+zehayk@users.noreply.github.com>
2023-12-15 14:26:04 -0500

    gearbox added

--- CarRunner/Assets/Scripts/Playable/CarController.cs
+++ CarRunner/Assets/Scripts/Playable/CarController.cs
@@ -29,6 +29,7 @@ public class CarController : MonoBehaviour
     // Center of mass
     public Vector3 com;
     public Rigidbody rb;
+    public GearBox gearBox = new GearBox();
 
     void Start()
     {
@@ -49,16 +50,30 @@ public class CarController : MonoBehaviour
         float timeDifference = currentTime - startTime;
         points += ((timeDifference * speed)/1000);
         PointsText.text = points.ToString("F2") + "$";
+
+        if (Controls.upShift())
+        {
+            Debug.Log("waaa");
+            gearBox.upShift();
+        }
+        else if (Controls.downShift())
+        {
+            gearBox.downShift();
+        }
     }
+
     void FixedUpdate()
     {
         float speed = rb.velocity.magnitude;
         if (enabledMotor)
         {
-            float motor = maxMotorTorque * Controls.rightMinusLeftTrig();
-            float brakes = maxBrakeTorque * Controls.leftShoulder();
+            float motor = maxMotorTorque * Controls.rightTrig() * ((gearBox.currentGear < 1) ? -1 : 1);
+            // float brakes = maxBrakeTorque * Controls.leftShoulder();
+            float brakes = maxBrakeTorque * Controls.leftTrig();
             float handBrake = maxHandBrakeTorque * Controls.westbutton();
             float steering = maxSteeringAngle * Controls.leftStick();
+
+            
 
             foreach (AxleInfo axleInfo in axleInfos)
             {
@@ -71,7 +86,7 @@ public class CarController : MonoBehaviour
                 if (axleInfo.motor)
                 {
                     float currentSpeed = rb.velocity.magnitude;
-                    if (currentSpeed <= MaxSpeed)
+                    if (currentSpeed <= gearBox.maxSpeed())
                     {
                         axleInfo.leftWheel.motorTorque = motor;
                         axleInfo.rightWheel.motorTorque = motor;
@@ -100,7 +115,7 @@ public class CarController : MonoBehaviour
         Vector3 cameraDistance = Vector3.Lerp(startCamDistance, endCameraDistance, speed * cameraDistanceMultiplier);
         mainCamera.transform.localPosition = cameraDistance;
 
-        speedText.text = "Speed: " + (speed * 3.6f).ToString("F0") + " Km/h";
+        speedText.text = "Speed: " + (speed * 3.6f).ToString("F0") + " Km/h" + " G: " + ((gearBox.currentGear < 1) ? "R" : gearBox.currentGear);
     }
 
 
@@ -155,9 +170,61 @@ public class basicController
 
     public bool isPresent() => gamepad != null;
 
-    public float rightMinusLeftTrig() => (isPresent()) ? gamepad.rightTrigger.ReadValue() - gamepad.leftTrigger.ReadValue() : Input.GetAxis("Vertical");
+    // public float rightMinusLeftTrig() => (isPresent()) ? gamepad.rightTrigger.ReadValue() - gamepad.leftTrigger.ReadValue() : Input.GetAxis("Vertical");
+    public float rightTrig() => (isPresent()) ? gamepad.rightTrigger.ReadValue() : Input.GetAxis("Vertical");
+    public float leftTrig() => (isPresent()) ? gamepad.leftTrigger.ReadValue() : Input.GetAxis("Vertical");
     public float leftStick() => (isPresent()) ? gamepad.leftStick.x.ReadValue() : Input.GetAxis("Horizontal");
-    public int leftShoulder() => ((isPresent()) ? gamepad.leftShoulder.isPressed : Input.GetKey(KeyCode.Space)) ? 1 : 0;
+    // public int leftShoulder() => ((isPresent()) ? gamepad.leftShoulder.isPressed : Input.GetKey(KeyCode.Space)) ? 1 : 0;
+    public bool downShift() => (Input.GetKeyDown("q") || gamepad.leftShoulder.wasPressedThisFrame);
+    public bool upShift() => (Input.GetKeyDown("e") || gamepad.rightShoulder.wasPressedThisFrame);
     public float[] rightStick() => new float[] { (isPresent()) ? gamepad.rightStick.x.ReadValue() : Input.GetAxis("Mouse X"), (isPresent()) ? gamepad.rightStick.y.ReadValue() : Input.GetAxis("Mouse Y") };
     public int westbutton() => (isPresent() && gamepad.buttonWest.isPressed) ? 1 : 0;
+}
+
+public class GearBox 
+{
+    public int currentGear = 1;
+    public int maxGears = 5;
+    public GearBox()
+    {
+    }
+
+    public void upShift() 
+    {
+        if (this.currentGear < this.maxGears)
+        {
+            this.currentGear++;
+        } 
+    }
+
+    public void downShift() 
+    {
+        if (this.currentGear > 0)
+        {
+            this.currentGear--;
+        }
+    }
+
+    public float maxSpeed() 
+    {
+        switch (currentGear)
+        {
+            case 0: // this is reverse
+                return 40;
+            case 1:
+                return 30;
+            case 2:
+                return 60;
+            case 3:
+                return 90;
+            case 4:
+                return 120;
+            case 5:
+                return 150;
+            case 6:
+                return 180;
+            default:
+                return 0;
+        }
+    }
 }
